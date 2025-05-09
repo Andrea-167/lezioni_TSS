971359ec508790a2a5779e44703f1c029fbfd05f
Andrea-167 <andrea.mancuso@edu.immaginazioneelavoro.it>
2025-05-09 12:53:54 +0200

    Add files via upload

---
+++ Prj_2_ContoCorrente/src/Esercizio/Tester.java
@@ -0,0 +1,30 @@
+package Esercizio;
+
+import java.util.Scanner;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		
+		ContoCorrente CC1 = new ContoCorrente();
+		Scanner mioScann = new Scanner(System.in);
+		char scelta;
+		
+		Scanner name = new Scanner(System.in);
+		System.out.println("Salve, inserisci il tuo nome");
+		String nome = name.nextLine();
+		
+		Scanner surname = new Scanner(System.in);
+		System.out.println("Salve, inserisci il tuo cognome");
+		String cognome = surname.nextLine();
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
