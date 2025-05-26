b94946bdbf50bdc5533742c2b21ea83aab79fde1
Andrea-167 <andrea.mancuso@edu.immaginazioneelavoro.it>
2025-05-26 14:22:04 +0200

    Add files via upload

---
+++ LezioniTeoriche/LezioniTeoriche/src/Lezione06_Aggregazione/Libro.java
@@ -0,0 +1,14 @@
+package Lezione06_Aggregazione;
+
+public class Libro {
+
+	String titolo;
+	Autore autore;
+	
+	public Libro(String titolo, Autore autore) {
+		this.titolo = titolo;
+		this.autore = autore;
+	}
+	
+	
+}
